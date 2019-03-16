13b209a971fd4a460ffa6e66aec214d008a6e50b
Lukasz Okraszewski <neo1936@gmail.com>
2019-03-16 19:24:20 +0000

    Fixed clang compile problem

--- test/timeout_test.cpp
+++ test/timeout_test.cpp
@@ -17,7 +17,8 @@
 #include "timeout/timer.h"
 
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
-
+#define CATCH_INTERNAL_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS
+#define CATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS
 #include <catch2/catch.hpp>
 
 static auto l_log = spdlog::stdout_color_mt("catch");
